0413a384c15f31a3055657a13eedc75dcdfdf3f0
kennycaiguo <61092549+kennycaiguo@users.noreply.github.com>
2020-02-25 18:10:43 -0600

    Add files via upload

---
+++ vbnethttpUpload/vbnethttpUpload/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox_file = New System.Windows.Forms.TextBox()
+        Me.BtnBrowse = New System.Windows.Forms.Button()
+        Me.btnUpload = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(23, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "File name:"
+        '
+        'TextBox_file
+        '
+        Me.TextBox_file.Location = New System.Drawing.Point(88, 38)
+        Me.TextBox_file.Name = "TextBox_file"
+        Me.TextBox_file.Size = New System.Drawing.Size(163, 21)
+        Me.TextBox_file.TabIndex = 1
+        '
+        'BtnBrowse
+        '
+        Me.BtnBrowse.Location = New System.Drawing.Point(271, 35)
+        Me.BtnBrowse.Name = "BtnBrowse"
+        Me.BtnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.BtnBrowse.TabIndex = 2
+        Me.BtnBrowse.Text = "Browse"
+        Me.BtnBrowse.UseVisualStyleBackColor = True
+        '
+        'btnUpload
+        '
+        Me.btnUpload.Location = New System.Drawing.Point(88, 103)
+        Me.btnUpload.Name = "btnUpload"
+        Me.btnUpload.Size = New System.Drawing.Size(93, 23)
+        Me.btnUpload.TabIndex = 3
+        Me.btnUpload.Text = "Upload"
+        Me.btnUpload.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(25, 153)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(321, 77)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(409, 290)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnUpload)
+        Me.Controls.Add(Me.BtnBrowse)
+        Me.Controls.Add(Me.TextBox_file)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox_file As System.Windows.Forms.TextBox
+    Friend WithEvents BtnBrowse As System.Windows.Forms.Button
+    Friend WithEvents btnUpload As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ vbnethttpUpload/vbnethttpUpload/Form1.vb
@@ -0,0 +1,55 @@
+﻿Imports System.Net
+Imports System.Net.Mime
+Imports InetCtlsObjects
+
+Public Class Form1
+    Dim filename As String
+    Private Sub BtnBrowse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnBrowse.Click
+        Dim ofd As New OpenFileDialog
+        ofd.Filter = "text files(*.txt)|*.txt|all files(*.*)|*.*"
+        ofd.InitialDirectory = Application.StartupPath
+        If DialogResult.OK = ofd.ShowDialog Then
+            TextBox_file.Text = ofd.FileName
+            filename = ofd.SafeFileName()
+            MsgBox(filename)
+        End If
+    End Sub
+
+    Private Sub btnUpload_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnUpload.Click
+        If TextBox_file.Text.Trim = "" Then
+            MsgBox("Please Enter Or Choose File")
+            TextBox_file.Focus()
+        Else
+            '' My.Computer.Network.UploadFile(TextBox_file.Text, "http://localhost/Default.aspx")
+            '' My.Computer.Network.UploadFile(TextBox_file.Text, "https://fishc.com.cn/forum-84-1.html", "kennycai2018", "Kenny197512")
+            ''My.Computer.Network.UploadFile(TextBox_file.Text, "https://www.dropbox.com/home/index.html", "kenny887@gmail.com", "kenny1975")
+
+            'Dim client As New WebClient()
+            'client.Headers.Add("Connect_Type", "application/x-www-form-urlencoded")
+            ''client.Credentials = New NetworkCredential("kennycai2018", "Kenny197512")
+
+            ''client.UploadFile("https://fishc.com.cn/forum-84-1.html", TextBox_file.Text)
+            ''client.UploadFile("https://fishc.com.cn/home.php", TextBox_file.Text)
+            ''client.Credentials = New NetworkCredential("kenny887@gmail.com", "kenny1975")
+            ''client.UploadFile("https://www.dropbox.com/home/", TextBox_file.Text)
+            'client.Credentials = New NetworkCredential("kenny887@gmail.com", "kenny1975")
+            ''client.UploadFile("https://www.youtube.com/channel/UCGFF66nQkLHrTXkXS6DN7yw/videos?view=0&sort=dd&shelf_id=0", TextBox_file.Text)
+            ''client.UploadFile("https://www.youtube.com/upload?redirect_to_creator=true&fr=2", TextBox_file.Text)
+            ''client.UploadFile("https://drive.google.com/drive/u/0/folders/18y_2OMe84f_5PaWJMfHBn5Rsp13f1YZa", TextBox_file.Text)
+            'client.UploadFile("https://drive.google.com/drive/u/0/folders/1rDVq6LscFlSFZx4JDlOvq1AVr6PSf0BX", "POST", TextBox_file.Text)
+
+            'client.Dispose()
+            Dim inet1 As InetCtlsObjects.Inet = New InetCtlsObjects.Inet()
+            'inet1.URL = "https://drive.google.com/drive/u/0/folders/1rDVq6LscFlSFZx4JDlOvq1AVr6PSf0BX"
+            'inet1.UserName = "kenny887@gmail.com"
+            'inet1.Password = "kenny1975"
+
+            'TextBox1.Text = inet1.OpenURL("https://drive.google.com/drive/u/0/folders/1nuQZ3NkIsBACaBoxExea_v_p9aUqlmQB")
+            'inet1.Execute("https://drive.google.com/drive/u/0/folders/1nuQZ3NkIsBACaBoxExea_v_p9aUqlmQB", "POST", TextBox_file.Text)
+            TextBox1.Text = inet1.OpenURL("http://localhost/Default.aspx")
+            inet1.Execute("http://localhost/up/", "PUT", TextBox_file.Text)
+            'My.Computer.Network.UploadFile(TextBox_file.Text, "http://localhost/Default.aspx")
+        End If
+
+    End Sub
+End Class
