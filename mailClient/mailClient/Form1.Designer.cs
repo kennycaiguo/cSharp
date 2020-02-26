e3cc384813e984f913c59d3980cdae62389f724b
kennycaiguo <61092549+kennycaiguo@users.noreply.github.com>
2020-02-25 18:09:06 -0600

    Add files via upload

---
+++ mailClient/mailClient/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace mailClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_file = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labSize = new System.Windows.Forms.Label();
+            this.labState = new System.Windows.Forms.Label();
+            this.labSpeed = new System.Windows.Forms.Label();
+            this.labupSize = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter File Name:";
+            // 
+            // textBox_file
+            // 
+            this.textBox_file.Location = new System.Drawing.Point(131, 36);
+            this.textBox_file.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox_file.Name = "textBox_file";
+            this.textBox_file.Size = new System.Drawing.Size(208, 21);
+            this.textBox_file.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(359, 32);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(119, 24);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 104);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Upload Progress:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(137, 104);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(193, 15);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(359, 98);
+            this.btnUpload.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(119, 24);
+            this.btnUpload.TabIndex = 5;
+            this.btnUpload.Text = "UpLoad";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 148);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Time Used:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(259, 148);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "average speed:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(73, 184);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "UploadState:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(259, 184);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 12);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Upload Size:";
+            // 
+            // labSize
+            // 
+            this.labSize.AutoSize = true;
+            this.labSize.Location = new System.Drawing.Point(357, 184);
+            this.labSize.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labSize.Name = "labSize";
+            this.labSize.Size = new System.Drawing.Size(41, 12);
+            this.labSize.TabIndex = 10;
+            this.labSize.Text = "label7";
+            // 
+            // labState
+            // 
+            this.labState.AutoSize = true;
+            this.labState.Location = new System.Drawing.Point(169, 184);
+            this.labState.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labState.Name = "labState";
+            this.labState.Size = new System.Drawing.Size(41, 12);
+            this.labState.TabIndex = 11;
+            this.labState.Text = "label8";
+            // 
+            // labSpeed
+            // 
+            this.labSpeed.AutoSize = true;
+            this.labSpeed.Location = new System.Drawing.Point(363, 148);
+            this.labSpeed.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labSpeed.Name = "labSpeed";
+            this.labSpeed.Size = new System.Drawing.Size(41, 12);
+            this.labSpeed.TabIndex = 12;
+            this.labSpeed.Text = "label9";
+            // 
+            // labupSize
+            // 
+            this.labupSize.AutoSize = true;
+            this.labupSize.Location = new System.Drawing.Point(163, 148);
+            this.labupSize.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labupSize.Name = "labupSize";
+            this.labupSize.Size = new System.Drawing.Size(47, 12);
+            this.labupSize.TabIndex = 13;
+            this.labupSize.Text = "label10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 279);
+            this.Controls.Add(this.labupSize);
+            this.Controls.Add(this.labSpeed);
+            this.Controls.Add(this.labState);
+            this.Controls.Add(this.labSize);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.textBox_file);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_file;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labSize;
+        private System.Windows.Forms.Label labState;
+        private System.Windows.Forms.Label labSpeed;
+        private System.Windows.Forms.Label labupSize;
+    }
+}
+
---
+++ mailClient/mailClient/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+ 
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Net;
+ 
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace mailClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string filePath = "";
+        string strfilename;
+         
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd =new OpenFileDialog();
+            if (DialogResult.OK == ofd.ShowDialog())
+            {
+                textBox_file.Text = ofd.FileName;
+                filePath = textBox_file.Text.Trim();
+               int pos= filePath.LastIndexOf("\\");
+               strfilename = filePath.Substring(pos+1);
+               MessageBox.Show(strfilename );
+            }
+            else
+            {
+                MessageBox.Show("No File Selected");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public int UpLoadFile(string address, string fileNamePath, string saveName, ProgressBar progressBar)
+        {
+            int returnValue = 0;
+            //要上传的文件
+            FileStream fs = new FileStream(fileNamePath, FileMode.Open, FileAccess.Read);
+            //二进制对象
+            BinaryReader r = new BinaryReader(fs);
+            //时间戳
+            string strBoundary = "----------" + DateTime.Now.Ticks.ToString("x");
+            byte[] boundaryBytes = Encoding.ASCII.GetBytes("\r\n--" + strBoundary + "\r\n");
+            //请求的头部信息
+            StringBuilder sb = new StringBuilder();
+            sb.Append("--");
+            sb.Append(strBoundary);
+            sb.Append("\r\n");
+            sb.Append("Content-Disposition: form-data; name=\"");
+            sb.Append("file");
+            sb.Append("\"; filename=\"");
+            sb.Append(saveName);
+            sb.Append("\";");
+            sb.Append("\r\n");
+            sb.Append("Content-Type: ");
+            sb.Append("application/octet-stream");
+            sb.Append("\r\n");
+            sb.Append("\r\n");
+            string strPostHeader = sb.ToString();
+            byte[] postHeaderBytes = Encoding.UTF8.GetBytes(strPostHeader);
+            // 根据uri创建HttpWebRequest对象   
+            HttpWebRequest httpReq = (HttpWebRequest)WebRequest.Create(new Uri(address));
+            httpReq.Method = "POST";
+            //对发送的数据不使用缓存   
+            httpReq.AllowWriteStreamBuffering = false;
+            //设置获得响应的超时时间（300秒）   
+            httpReq.Timeout = 300000;
+            httpReq.ContentType = "multipart/form-data; boundary=" + strBoundary;
+            long length = fs.Length + postHeaderBytes.Length + boundaryBytes.Length;
+            long fileLength = fs.Length;
+            httpReq.ContentLength = length;
+            
+                progressBar.Maximum = int.MaxValue;
+                progressBar.Minimum = 0;
+                progressBar.Value = 0;
+                //每次上传4k  
+                int bufferLength = 4096;
+                byte[] buffer = new byte[bufferLength]; //已上传的字节数   
+                long offset = 0;         //开始上传时间   
+                DateTime startTime = DateTime.Now;
+                int size = r.Read(buffer, 0, bufferLength);
+                Stream postStream = httpReq.GetRequestStream();         //发送请求头部消息   
+                postStream.Write(postHeaderBytes, 0, postHeaderBytes.Length);
+                while (size > 0)
+                {
+                    postStream.Write(buffer, 0, size);
+                    offset += size;
+                    progressBar.Value = (int)(offset * (int.MaxValue / length));
+                    TimeSpan span = DateTime.Now - startTime;
+                    double second = span.TotalSeconds;
+                    labSize.Text = "已用时：" + second.ToString("F2") + "秒";
+                    if (second > 0.001)
+                    {
+                        labSpeed.Text = "平均速度：" + (offset / 1024 / second).ToString("0.00") + "KB/秒";
+                    }
+                    else
+                    {
+                        labSpeed.Text = " 正在连接…";
+                    }
+                    labState.Text = "已上传：" + (offset * 100.0 / length).ToString("F2") + "%";
+                    labSize.Text = (offset / 1048576.0).ToString("F2") + "M/" + (fileLength / 1048576.0).ToString("F2") + "M";
+                    Application.DoEvents();
+                    size = r.Read(buffer, 0, bufferLength);
+                }
+                //添加尾部的时间戳   
+                postStream.Write(boundaryBytes, 0, boundaryBytes.Length);
+                postStream.Close();
+                //获取服务器端的响应   
+                WebResponse webRespon = httpReq.GetResponse();
+                Stream s = webRespon.GetResponseStream();
+                //读取服务器端返回的消息  
+                StreamReader sr = new StreamReader(s);
+                String sReturnString = sr.ReadLine();
+                s.Close();
+                sr.Close();
+                if (sReturnString == "Success")
+                {
+                    returnValue = 1;
+                }
+                else if (sReturnString == "Error")
+                {
+                    returnValue = 0;
+                }
+            
+             
+                fs.Close();
+                r.Close();
+            
+            return returnValue;
+        }
+
+        
+        private void Upload(string address, string fileNamePath, string saveName)
+        {
+            WebClient webClient = new WebClient();
+            //使用Windows登录方式
+            webClient.Credentials = new NetworkCredential("kennycai", "123");
+            
+            //上传的链接地址（文件服务器）
+            Uri uri = new Uri(address + saveName);
+            MessageBox.Show(uri.ToString());
+            webClient.UploadFileAsync(uri, "PUT", filePath);
+            webClient.Dispose();
+        }
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+
+            //上传服务器的地址（web服务）
+            
+            string address = "http://localhost/";//前提是已经配置好了IIS并且部署了网页aspx
+            //上传后文件保存的名称
+            string saveName = strfilename;
+            int count = UpLoadFile(address, filePath, saveName, this.progressBar1);
+
+
+            if (count == 1)
+            {
+                MessageBox.Show("success");
+            }
+            else
+            {
+                MessageBox.Show("Failed");
+            }
+
+            //Upload(address, filePath, saveName);
+        }
+
+}
+
+}
+
